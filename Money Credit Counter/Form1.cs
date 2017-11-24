e0116f2ebda3ff6e68f0ee797d27d2c9437d7f76
BEN KATA <b7013694@hallam.shu.ac.uk>
2017-11-24 19:06:09 +0000

    Added the source code to the rest of the coins and changed the data accordingly

--- Money Credit Counter/Form1.cs
+++ Money Credit Counter/Form1.cs
@@ -61,7 +61,20 @@ namespace Money_Credit_Counter
 
         private void btn_2p_Click(object sender, EventArgs e)
         {
-           
+            if (creditcost == 0) // If statement checking if the credit per cost has been entered
+            {
+                MessageBox.Show("Please enter the amount of credit"); // If not, shows this message
+            }
+            else // Otherwise
+            {
+
+                Total_pence += 2; // adds 2p to the total pence value
+                Total_Pound += 0.02; // adds 2p to the total pound value 
+                count_2p += 1; // adds 1 to the count of the coin
+            }
+            lbl_2p.Text = count_2p.ToString(); // displays the count on the label for the coin
+            txt_Totalpence.Text = Total_pence.ToString(); // displays the total in pence
+            txt_Totalpound.Text = Total_Pound.ToString(); // displays the total in pounds
         }
 
         private void btn_Credit_Click(object sender, EventArgs e)
@@ -71,30 +84,108 @@ namespace Money_Credit_Counter
 
         private void btn_5p_Click(object sender, EventArgs e)
         {
-            
+            if (creditcost == 0) // If statement checking if the credit per cost has been entered
+            {
+                MessageBox.Show("Please enter the amount of credit"); // If not, shows this message
+            }
+            else // Otherwise
+            {
+
+                Total_pence += 5; // adds 5p to the total pence value
+                Total_Pound += 0.05; // adds 5p to the total pound value 
+                count_5p += 1; // adds 1 to the count of the coin
+            }
+            lbl_5p.Text = count_5p.ToString(); // displays the count on the label for the coin
+            txt_Totalpence.Text = Total_pence.ToString(); // displays the total in pence
+            txt_Totalpound.Text = Total_Pound.ToString(); // displays the total in pounds
         }
 
         private void btn_10p_Click(object sender, EventArgs e)
         {
-           
+            if (creditcost == 0) // If statement checking if the credit per cost has been entered
+            {
+                MessageBox.Show("Please enter the amount of credit"); // If not, shows this message
+            }
+            else // Otherwise
+            {
+
+                Total_pence += 10; // adds 10p to the total pence value
+                Total_Pound += 0.10; // adds 10p to the total pound value 
+                count_10p += 1; // adds 1 to the count of the coin
+            }
+            lbl_10p.Text = count_10p.ToString(); // displays the count on the label for the coin
+            txt_Totalpence.Text = Total_pence.ToString(); // displays the total in pence
+            txt_Totalpound.Text = Total_Pound.ToString(); // displays the total in pounds
         }
 
         private void btn_20p_Click(object sender, EventArgs e)
         {
-            
+            if (creditcost == 0) // If statement checking if the credit per cost has been entered
+            {
+                MessageBox.Show("Please enter the amount of credit"); // If not, shows this message
+            }
+            else // Otherwise
+            {
+
+                Total_pence += 20; // adds 20p to the total pence value
+                Total_Pound += 0.20; // adds 20p to the total pound value 
+                count_20p += 1; // adds 1 to the count of the coin
+            }
+            lbl_20p.Text = count_20p.ToString(); // displays the count on the label for the coin
+            txt_Totalpence.Text = Total_pence.ToString(); // displays the total in pence
+            txt_Totalpound.Text = Total_Pound.ToString(); // displays the total in pounds
         }
 
         private void btn_50p_Click(object sender, EventArgs e)
         {
-            
+            if (creditcost == 0) // If statement checking if the credit per cost has been entered
+            {
+                MessageBox.Show("Please enter the amount of credit"); // If not, shows this message
+            }
+            else // Otherwise
+            {
+
+                Total_pence += 50; // adds 50p to the total pence value
+                Total_Pound += 0.50; // adds 50p to the total pound value 
+                count_50p += 1; // adds 1 to the count of the coin
+            }
+            lbl_50p.Text = count_50p.ToString(); // displays the count on the label for the coin
+            txt_Totalpence.Text = Total_pence.ToString(); // displays the total in pence
+            txt_Totalpound.Text = Total_Pound.ToString(); // displays the total in pounds
         }
         private void btn_1pound_Click(object sender, EventArgs e)
         {
+            if (creditcost == 0) // If statement checking if the credit per cost has been entered
+            {
+                MessageBox.Show("Please enter the amount of credit"); // If not, shows this message
+            }
+            else // Otherwise
+            {
 
+                Total_pence += 100; // adds 100p to the total pence value
+                Total_Pound += 1.00; // adds £1 to the total pound value 
+                count_1pound += 1; // adds 1 to the count of the coin
+            }
+            lbl_1pound.Text = count_1pound.ToString(); // displays the count on the label for the coin
+            txt_Totalpence.Text = Total_pence.ToString(); // displays the total in pence
+            txt_Totalpound.Text = Total_Pound.ToString(); // displays the total in pounds
         }
         private void btn_2pound_Click(object sender, EventArgs e)
         {
+            if (creditcost == 0) // If statement checking if the credit per cost has been entered
+            {
+                MessageBox.Show("Please enter the amount of credit"); // If not, shows this message
+            }
+            else // Otherwise
+            {
 
+                Total_pence += 200; // adds 200p to the total pence value
+                Total_Pound += 2.00; // adds £2 to the total pound value 
+                count_2pound += 1; // adds 1 to the count of the coin
+            }
+            lbl_2pound.Text = count_2pound.ToString(); // displays the count on the label for the coin
+            txt_Totalpence.Text = Total_pence.ToString(); // displays the total in pence
+            txt_Totalpound.Text = Total_Pound.ToString(); // displays the total in pounds
         }
         private void btn_reset_Click(object sender, EventArgs e) 
         {
