80b27158f7a7e8d799ed0a2840d3935320e0e4c9
BEN KATA <b7013694@hallam.shu.ac.uk>
2017-11-24 18:38:28 +0000

    Added all the Global variables I need

--- Money Credit Counter/Form1.cs
+++ Money Credit Counter/Form1.cs
@@ -14,7 +14,18 @@ namespace Money_Credit_Counter
     public partial class Form1 : Form
     {
 
-        
+        int Total_pence = 0;
+        Double Total_Pound = 0.00;
+        int count_1p = 0;
+        int count_2p = 0;
+        int count_5p = 0;
+        int count_10p = 0;
+        int count_20p = 0;
+        int count_50p = 0;
+        int count_1pound = 0;
+        int count_2pound = 0;
+        int creditcost;
+
 
 
 
